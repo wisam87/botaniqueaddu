036b242fe4de5a2e7a3e161036e6f3027cd24774
Wisam <wisam878787@gmail.com>
2018-11-24 17:01:18 +0500

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Botanique Addu</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+       
+    </head>
+    <body>
+            <div style="text-align: center;">
+                    <img src="/images/botaniquelogo.jpg" style="width: 50%;">
+                    <h1 style="font-family: sans-serif;">Coming Soon</h1>
+            </div>
+
+    </body>
+</html>
